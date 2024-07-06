4f957d2cc21d0dd58bb845aa7e2343f985005bf1
azurealstn <55525868+azurealstn@users.noreply.github.com>
2024-07-06 21:02:26 +0900

    JS-Header

---
+++ src/main.js
@@ -0,0 +1,17 @@
+/**
+ * Header에 페이지 아래로 스크롤시 다크 스타일링 적용
+ */
+const header = document.querySelector('.header');
+const headerHeight = header.getBoundingClientRect().height;
+
+document.addEventListener('scroll', (e) => {
+  const scrollY = window.scrollY;
+  console.log(scrollY);
+  if (scrollY > headerHeight) {
+    header.classList.add('header--dark');
+  } else {
+    header.classList.remove('header--dark');
+  }
+});
+
+
